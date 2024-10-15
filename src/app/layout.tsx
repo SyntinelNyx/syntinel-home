9dcb5fe9fa4108b05f56545e89955980d829e37d
SapphireGaze <jasminehuang2048@outlook.com>
2024-10-14 23:12:36 -0700

    Added Umami Analytics: - Added Umami analytics script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          defer
+          src="https://umami.miguelmuniz.com/script.js"
+          data-website-id="644801db-2cf2-4f97-a607-784b39440b26"
+        ></script>
+      </head>
       <body className={montserrat.className}>
         <ThemeProviderWrapper>
           <Box component="main">
